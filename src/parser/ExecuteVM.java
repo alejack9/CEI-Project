04ca4d80c90b39f8e9b58b90509d25b014525200
a.giacche <alessandrogiacche@gmail.com>
2020-08-24 19:29:08 +0200

    simple code cleanup

--- src/parser/ExecuteVM.java
+++ src/parser/ExecuteVM.java
@@ -31,25 +31,20 @@ public class ExecuteVM {
 			int v1;
 			
 			switch (bytecode) {
-			
 			case 0:
 				return;
-
 			case SVMParser.LOADINTEGER:
 				regs[code[ip++]] = code[ip++];
 				break;
-
 			case SVMParser.BRANCH:
 				ip = code[ip];
 				break;
-
 			case SVMParser.PUSH:
 				setRegValue(Regs.$sp, getRegValue(Regs.$sp) - code[ip + 1]);
 				for(v1 = 0; v1 < code[ip + 1]; v1++)
 					memory[getRegValue(Regs.$sp) + v1] = decToByte(regs[code[ip]], (byte) (code[ip+1] - v1 - 1));
 				ip += 2;
 				break;
-
 			case SVMParser.LOADWORD:
 				if (code[ip + 3] == 1) 
 					regs[code[ip]] = byteToDec((byte) 0,(byte) 0, (byte) 0, memory[code[ip + 1] + regs[code[ip + 2]]]);
@@ -58,73 +53,52 @@ public class ExecuteVM {
 							memory[code[ip + 1] + regs[code[ip + 2]]],
 							memory[code[ip + 1] + regs[code[ip + 2]] + 1],
 							memory[code[ip + 1] + regs[code[ip + 2]] + 2],
-							memory[code[ip + 1] + regs[code[ip + 2]] + 3] );
+							memory[code[ip + 1] + regs[code[ip + 2]] + 3]
+					);
 				ip += 4;
 				break;
-
 			case SVMParser.STOREWORD:
 				for(v1 = 0; v1 < code[ip + 3]; v1++)
 					memory[code[ip + 1] + regs[code[ip + 2]] + v1] = decToByte(regs[code[ip]], (byte) (code[ip + 3] - v1 - 1));
 				ip += 4;
 				break;
-
 			case SVMParser.ADD:
 				regs[code[ip++]] = regs[code[ip++]] + regs[code[ip++]];
 				break;
-
 			case SVMParser.SUB:
 				regs[code[ip++]] = regs[code[ip++]] - regs[code[ip++]];
 				break;
-
 			case SVMParser.MUL:
 				regs[code[ip++]] = regs[code[ip++]] * regs[code[ip++]];
 				break;
-
 			case SVMParser.DIV:
 				regs[code[ip++]] = regs[code[ip++]] / regs[code[ip++]];
 				break;
-
 			case SVMParser.POP:
-//				if (code[ip] == 1)
-//					setRegValue(Regs.$a0, byteToDec((byte) 0,(byte) 0, (byte) 0, memory[getRegValue(Regs.$sp)]));
-//				else
-//					regs[code[ip]] = byteToDec(memory[code[ip + 1] + regs[code[ip + 2]]], memory[code[ip + 1] + regs[code[ip + 2]] + 1], memory[code[ip + 1] + regs[code[ip + 2]] + 2], memory[code[ip + 1] + regs[code[ip + 2]] + 3] );
 				setRegValue(Regs.$sp, getRegValue(Regs.$sp) + code[ip++] );
 				break;
-
 			case SVMParser.BRANCHEQ:
-				if (regs[code[ip++]] == regs[code[ip++]])
-					ip = code[ip];
-				else
-					ip++;
+				if (regs[code[ip++]] == regs[code[ip++]]) ip = code[ip];
+				else ip++;
 				break;
-
 			case SVMParser.BRANCHGT:
-				if (regs[code[ip++]] > regs[code[ip++]])
-					ip = code[ip];
-				else
-					ip++;
+				if (regs[code[ip++]] > regs[code[ip++]]) ip = code[ip];
+				else ip++;
 				break;
 
 			case SVMParser.BRANCHGE:
-				if (regs[code[ip++]] >= regs[code[ip++]])
-					ip = code[ip];
-				else
-					ip++;
+				if (regs[code[ip++]] >= regs[code[ip++]]) ip = code[ip];
+				else ip++;
 				break;
 
 			case SVMParser.BRANCHLT:
-				if (regs[code[ip++]] < regs[code[ip++]])
-					ip = code[ip];
-				else
-					ip++;
+				if (regs[code[ip++]] < regs[code[ip++]]) ip = code[ip];
+				else ip++;
 				break;
 
 			case SVMParser.BRANCHLE:
-				if (regs[code[ip++]] <= regs[code[ip++]])
-					ip = code[ip];
-				else
-					ip++;
+				if (regs[code[ip++]] <= regs[code[ip++]]) ip = code[ip];
+				else ip++;
 				break;
 
 			case SVMParser.NEG:
@@ -153,13 +127,11 @@ public class ExecuteVM {
 				break;
 
 			default:
-				System.err.println("Unsupported bytecode");
+				System.err.println("Unsupported bytecode: " + bytecode);
 				return;
 			}
 		}
 	}
-	
-
 	
 	private int getRegValue(Regs reg) {
 		return regs[reg.ordinal()];
@@ -171,18 +143,17 @@ public class ExecuteVM {
 
 	
 	private static byte decToByte(int number, byte i) {
-		return (byte) (number >> 8*i);
+		return (byte) (number >> 8 * i);
 	}
 	
 	// 3 2 1 0
 	private static int byteToDec(byte... numbers) {
 		// "& 0xFF" turns signed number to binary value, so we can shift this without be aware of the sign
-		return ((numbers[0] & 0xFF) << 24)
+		return 	  ((numbers[0] & 0xFF) << 24)
 				| ((numbers[1] & 0xFF) << 16)
 				| ((numbers[2] & 0xFF) << 8)
 				| ((numbers[3] & 0xFF) << 0);	
 
 		
 	}
-
 }
