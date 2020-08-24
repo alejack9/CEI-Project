c3280acd6e47dcd3c530f94084d8e6f88832b1c4
a.giacche <alessandrogiacche@gmail.com>
2020-08-24 19:28:34 +0200

    massive code cleanup

--- src/main/Analyse.java
+++ src/main/Analyse.java
@@ -29,12 +29,122 @@ import util_analysis.entries.BTEntry;
 import util_analysis.entries.STEntry;
 
 public class Analyse {
+	
+	private final Logger logger;
+	private String inFileName = "test.spl";
+	private String errorsFileName = null;
+	private String outCodeFileName;
 
-	public static void main(String[] args) {
-		String inFileName = "test.spl";
-		String errorsFileName = "errors.txt";
-		String outCodeFileName;
+	private Analyse(Logger logger) {
+		this.logger = logger;
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Analyse(LoggerFactory.getLogger()).start(args);
+	}
+	
+	private void start(String[] args) throws IOException {
+		manipulateArgs(args);
+		FileInputStream is = new FileInputStream(inFileName);
+		ANTLRInputStream input = new ANTLRInputStream(is);
+	
+		logger.writeLine("Input File Name: " + inFileName);
+		logger.writeLine("Code File Name: " + outCodeFileName);
+	
+		logger.writeLine();
 		
+		SimplePlusLexer lexer = new SimplePlusLexer(input);
+		// disable default ANTLR lexer listener (to override default behavior)
+		lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
+		SimpleErrorListener sl = null;
+		if(errorsFileName != null) {
+			logger.writeLine("Errors File Name: " + errorsFileName);
+			 sl = new SimpleSintaxErrorListener(LoggerFactory.getLogger(errorsFileName));
+			lexer.addErrorListener(sl);
+		}
+		lexer.addErrorListener(new SimpleLexerErrorListener(logger));
+	
+		logger.writeLine("Creating Tokens Stream...");
+	
+		//create parser
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		logger.writeLine("...Tokens Stream Created");
+	
+		logger.writeLine();
+		
+		SimplePlusParser parser = new SimplePlusParser(tokens);
+	
+		// disable default ANTLR syntax listener (to override default behavior)
+		parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
+		if(errorsFileName != null)
+			parser.addErrorListener(sl);
+		parser.addErrorListener(new SimpleSintaxErrorListener(logger));
+	
+		//tell the parser to build the AST
+		parser.setBuildParseTree(true);
+		
+		//build custom visitor
+		VisitorImplSP visitor = new VisitorImplSP();
+		
+		//visit the root, this will recursively visit the whole tree
+		StmtBlock mainBlock = (StmtBlock) visitor.visitBlock(parser.block());
+		
+		if(lexer.errors.size() > 0) {
+			for (LexicalError error : lexer.errors)
+				logger.writeLine("Lexical Error: " + error.toString());
+			quit(logger);
+		}
+		
+		if (sl != null && sl.errorsDetected())
+			quit(logger);
+	
+		logger.writeLine("Checking semantic...");
+		
+		List<SemanticError> errors = mainBlock.checkSemantics(new ListOfMapEnv<STEntry>());
+		
+		if (errors.size() > 0) {
+			logger.writeLine("Check semantics FAILED");
+			for (SemanticError err : errors)
+				logger.writeLine(err.toString());
+			quit(logger);
+		} 
+		
+		logger.writeLine("Check semantic succeeded");
+	
+		logger.writeLine();
+		
+		logger.writeLine("Checking types...");
+			
+		mainBlock.inferType();
+		
+		if (TypeErrorsStorage.getTypeErrors().size() > 0) {
+			logger.writeLine("Type check FAILED");
+			for (TypeError err : TypeErrorsStorage.getTypeErrors())
+				logger.writeLine(err.toString());
+			quit(logger);
+		} 
+	
+		logger.writeLine("Type check succeeded");
+	
+		logger.writeLine();
+		
+		List<BehaviourError> bErrors = mainBlock.inferBehaviour(new ListOfMapEnv<BTEntry>());
+		
+		if (bErrors.size() > 0) {
+			logger.writeLine("Behavioural analysis FAILED");			
+			for(SemanticError bErr: bErrors)
+				logger.writeLine(bErr.toString());
+			quit(logger);
+		}
+		logger.writeLine("Behavioural analysis succeeded");
+		
+		generateOutCode(mainBlock.codeGen());
+	
+		runCode();
+	}
+
+	private void manipulateArgs(String[] args) {
 		switch (args.length) {
 		case 0:
 			break;
@@ -46,151 +156,34 @@ public class Analyse {
 			errorsFileName = args[1];
 			break;
 		default:
-			System.out.println("Usage: \"java -jar .\\exportedJar.jar inputfile outputfile\"");
+			System.out.println("Usage: \"java -jar .\\exportedJar.jar input_file [errors_file]\"");
 		}
-		
 		outCodeFileName = inFileName.replaceFirst("[.][^.]+$", "") + ".out";
-		
-		// create console logger
-		Logger clogger = LoggerFactory.getLogger();
-				
-		try{   
-			FileInputStream is = new FileInputStream(inFileName);
-			ANTLRInputStream input = new ANTLRInputStream(is);
-        
-
-			clogger.writeLine("Input File: " + inFileName);
-			clogger.writeLine("Output File: " + errorsFileName);
-
-			clogger.writeLine();
-			
-			//create lexer
-			SimplePlusLexer lexer = new SimplePlusLexer(input);
-			// disable default ANTLR lexer listener (to override default behavior)
-			lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
-			lexer.addErrorListener(new SimpleLexerErrorListener(LoggerFactory.getLogger(errorsFileName)));
-			lexer.addErrorListener(new SimpleLexerErrorListener(clogger));
-
-			
-			clogger.writeLine("Collecting Tokens...");
-
-			//create parser
-			CommonTokenStream tokens = new CommonTokenStream(lexer);
-			
-			clogger.writeLine("...Tokens collected");
-
-			clogger.writeLine();
-			
-			SimplePlusParser parser = new SimplePlusParser(tokens);
-			SimpleErrorListener sl = new SimpleSintaxErrorListener(LoggerFactory.getLogger(errorsFileName));
-			
-			// disable default ANTLR syntax listener (to override default behavior)
-			parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
-			// logger returned from LoggerFactory writes on errors.txt file
-			parser.addErrorListener(sl);
-			parser.addErrorListener(new SimpleSintaxErrorListener(clogger));
-
-			//tell the parser to build the AST
-			parser.setBuildParseTree(true);
-			
-			//build custom visitor
-			VisitorImplSP visitor = new VisitorImplSP();
-			
-			//visit the root, this will recursively visit the whole tree
-			StmtBlock mainBlock = (StmtBlock) visitor.visitBlock(parser.block());
-			
-			if(lexer.errors.size() > 0) {
-				for (LexicalError error : lexer.errors)
-					clogger.writeLine("Lexical Error: " + error.toString());
-				quit(clogger);
-			}
-			
-			if (sl.errorsDetected()) {
-				quit(clogger);
-			}
+	}
 	
-			clogger.writeLine("Checking semantic...");
-			
-			List<SemanticError> errors = mainBlock.checkSemantics(new ListOfMapEnv<STEntry>());
-			
-			if (errors.size() > 0) {
-				clogger.writeLine("Check semantics FAILED");
-				for (SemanticError err : errors)
-					clogger.writeLine(err.toString());
-				quit(clogger);
-			} 
-			
-			clogger.writeLine("Check semantic succeeded");
-
-			clogger.writeLine();
-			
-			clogger.writeLine("Checking types...");
-				
-			mainBlock.inferType();
-			
-			if (TypeErrorsStorage.getTypeErrors().size() > 0) {
-				clogger.writeLine("Type check FAILED");
-				for (TypeError err : TypeErrorsStorage.getTypeErrors())
-					clogger.writeLine(err.toString());
-				quit(clogger);
-			} 
-
-			clogger.writeLine("Type check succeeded");
-
-			clogger.writeLine();
-			
-			List<BehaviourError> bErrors = mainBlock.inferBehaviour(new ListOfMapEnv<BTEntry>());
-			
-			if (bErrors.size() > 0) {
-				clogger.writeLine("Behavioural analysis FAILED");			
-				for(SemanticError bErr: bErrors)
-					clogger.writeLine(bErr.toString());
-				quit(clogger);
-			}
-			clogger.writeLine("Behavioural analysis succeeded");
-			String code = mainBlock.codeGen();
-			LoggerFactory.getLogger(outCodeFileName).write(code.replaceFirst("\r\n", ""));
-			
-			is = new FileInputStream(outCodeFileName);
-			input = new ANTLRInputStream(is);
-       
-			clogger.writeLine("Input File: " + outCodeFileName);
-
-			clogger.writeLine();
-			
-			//create lexer
-			SVMLexer SVMlexer = new SVMLexer(input);
-
-			//create parser
-			tokens = new CommonTokenStream(SVMlexer);
-			
-			
-			SVMParser SVMparser = new SVMParser(tokens);
-			
-			//tell the parser to build the AST
-			parser.setBuildParseTree(true);
-			
-			//build custom visitor
-			VisitorImplSVM SVMVisitor = new VisitorImplSVM();
-			
-			//visit the root, this will recursively visit the whole tree
-			SVMVisitor.visitAssembly(SVMparser.assembly());
-			
-			new ExecuteVM(SVMVisitor.getCode()).cpu();
-			
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	private void generateOutCode(String code) throws IOException {
+		LoggerFactory.getLogger(outCodeFileName).write(code.replaceFirst("\r\n", ""));
+	}
+	
+	private void runCode() throws IOException {
+		SVMParser SVMparser = new SVMParser(
+				new CommonTokenStream(
+						new SVMLexer(
+								new ANTLRInputStream(
+										new FileInputStream(outCodeFileName)
+		))));
+		
+		SVMparser.setBuildParseTree(true);
+		
+		VisitorImplSVM SVMVisitor = new VisitorImplSVM();
+		
+		SVMVisitor.visitAssembly(SVMparser.assembly());
+		
+		new ExecuteVM(SVMVisitor.getCode()).cpu();
 	}
 
-	private static void quit(Logger clogger) {
-		try {
-			clogger.writeLine("Quitting compiling");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private void quit(Logger clogger) throws IOException {
+		clogger.writeLine("Quitting compiling");
 		System.exit(-1);
 	}
 }
