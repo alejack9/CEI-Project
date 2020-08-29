97fc76c44462838bae70f7c89d034135b189ed11
Alessandro Giacche' <alessandrogiacche@gmail.com>
2020-08-29 16:24:16 +0200

    added getVariableRefCodeGen

--- src/support/CodeGenUtils.java
+++ src/support/CodeGenUtils.java
@@ -10,6 +10,12 @@ public class CodeGenUtils {
 	}
 
 	public static String k;
+
+	private static void moveToCorrectAR(STEntry variable, int nl, CustomStringBuilder sb) {
+		sb.newLine("move $al $fp");
+		for (int i = 0; i < nl - variable.nestingLevel; i++)
+			sb.newLine("lw $al 0($al) 4");
+	}
 	
 	/**
 	 * Access to a variable in the same or external activation record.
@@ -20,11 +26,22 @@ public class CodeGenUtils {
 	 * @param sb - the string to add the assembly command
 	 */
 	public static void getVariableCodeGen(STEntry variable, int nl, CustomStringBuilder sb) {
-		sb.newLine("move $al $fp");
-		for (int i = 0; i < nl - variable.nestingLevel; i++)
-			sb.newLine("lw $al 0($al) 4");
+		moveToCorrectAR(variable, nl, sb);
 		sb.newLine("lw $a0 ", Integer.toString(variable.offset), "($al) ",
 				Integer.toString(variable.getType().getDimension()));
+	}
+	
+	/**
+	 * Access to a variable in the same or external activation record.
+	 * <br>Load in the register "$a0" the variable required.
+	 * 
+	 * @param variable - the variable to access
+	 * @param nl - the current nesting level
+	 * @param sb - the string to add the assembly command
+	 */
+	public static void getVariableRefCodeGen(STEntry variable, int nl, CustomStringBuilder sb) {
+		moveToCorrectAR(variable, nl, sb);
+		sb.newLine("addi $a0 $al ", Integer.toString(variable.offset));
 	}
 
 	/**
